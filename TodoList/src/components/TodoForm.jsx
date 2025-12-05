8bea3504edc3018e857f356498e6dc9812dbbecd
차영심 <chayoungsim@happyict.co.kr>
2025-12-05 16:52:43 +0900

    todolist 기능추가

--- TodoList/src/components/TodoForm.jsx
+++ TodoList/src/components/TodoForm.jsx
@@ -4,14 +4,23 @@ const TodoForm = ({onAddTodo}) => {
   const [input, setInput] = useState('');
 
     const clickAdd = () => {
-        onAddTodo(input)
-        setInput("");
+        if(input.trim() !=='') {
+            onAddTodo(input)
+            setInput("");
+        }        
     }
+
+    const handleKeyDown = (e) => {
+        if(e.key === "Enter") {
+            clickAdd();
+        }
+    }
+
   return (
     <div>
         <h2>Add Todo</h2>
         <div>
-            <input type="text" value={input} onChange={(e) => setInput(e.target.value)}/>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} onKeyDown={handleKeyDown}/>
             <button type="button" onClick={clickAdd}>Add</button>
         </div>
     </div>
