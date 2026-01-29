ce594d071608bf7ce5ea14f301eed52803513a1f
차영심 <chayoungsim@happyict.co.kr>
2026-01-29 17:39:51 +0900

    Ex-temp up

--- Ex-temp/src/App.jsx
+++ Ex-temp/src/App.jsx
@@ -4,7 +4,6 @@ import Calendar from "./components/Calendar"
 import Calendar2 from "./components/Calendar2"
 import Calendar3 from "./components/Calendar3"
 import TimePickerSample from './components/TimePicker'
-import InteractiveSlider from './components/interactive_slider'
 
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
 
   return (
     <>
-      <InteractiveSlider />
+      
       <Calendar />
       <Calendar2 />
       <Calendar3 />
